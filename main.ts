bb2f9cd184814ba12b0ed741653408843601acfc
bube009 <207951757+bube009@users.noreply.github.com>
2025-12-06 21:29:58 -0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { IonicVue } from '@ionic/vue';
+
+// CSS de base Ionic
+import '@ionic/vue/css/core.css';
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+import './theme/variables.css';
+
+const app = createApp(App);
+app.use(IonicVue);
+
+app.mount('#app');
