b70342a6c3e8bc942176c55006f9677b49793e19
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-07 20:48:15 -0500

    Delete src/src directory

--- src/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-import '@ionic/vue/css/core.css';
-
-createApp(App)
-  .use(router)
-  .mount('#app');
