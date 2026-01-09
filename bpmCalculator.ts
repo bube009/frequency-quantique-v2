079b3e29e6586e5cef1f215798134bfcaefc40b4
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-08 23:39:03 -0500

    Delete bpmCalculator.ts

--- bpmCalculator.ts
+++
@@ -1,9 +0,0 @@
-export function calculateBPM(peaks: number[]): number {
-  if (peaks.length < 2) return 0;
-  const intervals = [];
-  for (let i = 1; i < peaks.length; i++) {
-    intervals.push(peaks[i] - peaks[i - 1]);
-  }
-  const avg = intervals.reduce((a, b) => a + b) / intervals.length;
-  return Math.round(60000 / avg);
-}
