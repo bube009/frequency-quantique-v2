fa5a36fe40adbbd1b906a184c424ad999dc866d1
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-08 23:39:53 -0500

    Delete hrvAnalyzer.ts

--- hrvAnalyzer.ts
+++
@@ -1,6 +0,0 @@
-export function calculateHRV(intervals: number[]): number {
-  if (intervals.length < 2) return 0;
-  const mean = intervals.reduce((a, b) => a + b) / intervals.length;
-  const variance = intervals.reduce((a, b) => a + Math.pow(b - mean, 2), 0) / intervals.length;
-  return Math.sqrt(variance);
-}
