6cc103895bb29f9b98e308aacaf487577ec3c962
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-08 23:40:23 -0500

    Delete ppgProcessor.ts

--- ppgProcessor.ts
+++
@@ -1,7 +0,0 @@
-export function extractPPG(frame: ImageData): number {
-  let sum = 0;
-  for (let i = 0; i < frame.data.length; i += 4) {
-    sum += frame.data[i]; // canal rouge
-  }
-  return sum / (frame.data.length / 4);
-}
